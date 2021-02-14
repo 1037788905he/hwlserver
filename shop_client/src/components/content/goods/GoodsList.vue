ea86c9525ecf1ff60dab29327565fd77fa77adb6
he_wen_liang <1037788905@qq.com>
2021-02-15 00:55:49 +0800

    初始化项目，客户端

---
+++ shop_client/src/components/content/goods/GoodsList.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="goodslist">
+    <!-- :key="index" :goods_list_item="item" v-for="(item,index) in goods" -->
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+    <goods-list-item></goods-list-item>
+  </div>
+</template>
+
+<script>
+import GoodsListItem from "./GoodsListItem";
+export default {
+  name: "GoodsList",
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  components: {
+    GoodsListItem
+  }
+};
+</script>
+
+<style>
+.goodslist {
+  text-align: center;
+}
+</style>
---
+++ shop_client/src/views/detail/childComps/DetailSwiper.vue
@@ -0,0 +1,51 @@
+<template>
+  <swiper>
+    <swiper-item>
+      <img class="swiperdetail" @load="test" src="~assets/img/detail/model01.jpg" alt />
+    </swiper-item>
+    <swiper-item>
+      <img class="swiperdetail" src="~assets/img/detail/model02.jpg" alt />
+    </swiper-item>
+  </swiper>
+</template>
+
+<script>
+import { Swiper, SwiperItem } from "components/common/swiper";
+
+export default {
+  name: "DetialSwiper",
+  props: {
+    banners: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  },
+  data() {
+    return {
+      isLoad: true
+    };
+  },
+  created() {},
+  components: {
+    Swiper,
+    SwiperItem
+  },
+  methods: {
+    test() {
+      if (this.isLoad) {
+        this.$bus.$emit("test1");
+      }
+      this.isLoad = false;
+    }
+  },
+  mounted() {}
+};
+</script>
+
+<style scoped>
+.swiperdetail {
+  height: 250px;
+}
+</style>
